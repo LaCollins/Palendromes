5b8681639eb7f8ea09cb978b9858542631fb2b5a
Laura Collins <laura.e.collins1@gmail.com>
2020-04-15 18:37:49 -0500

    Add project files.

---
+++ Palendromes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Palendromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
